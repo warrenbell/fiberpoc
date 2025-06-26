4f749ef8a8bd333544c2e2e1206acf1f91451667
Warren Bell <warrenb@ideavo.com>
2025-06-26 10:59:11 -0600

    Update configs.go

--- common/models/configs.go
+++ common/models/configs.go
@@ -8,16 +8,24 @@ type Config interface {
 	GetLogLevel() *zapcore.Level
 	GetLogToFile() *bool
 	GetPostgresUrl() *string
+	GetGoogleOidcClientId() *string
+	GetGoogleOidcClientSecret() *string
+	GetGoogleOidcProviderUrl() *string
+	GetRedirectUri() *string
 }
 
 type AppConfig struct {
-	postgresUrl string        `env:"POSTGRESQL_URL,required"`
-	LogLevel    zapcore.Level `env:"LOG_LEVEL" envDefault:"debug"`
-	LogToFile   bool          `env:"LOG_TO_FILE" envDefault:"false"`
+	PostgresUrl            string        `env:"POSTGRESQL_URL,required"`
+	LogLevel               zapcore.Level `env:"LOG_LEVEL" envDefault:"debug"`
+	LogToFile              bool          `env:"LOG_TO_FILE" envDefault:"false"`
+	GoogleOidcClientId     string        `env:"GOOGLE_OIDC_CLIENT_ID,required"`
+	GoogleOidcClientSecret string        `env:"GOOGLE_OIDC_CLIENT_SECRET,required"`
+	GoogleOidcProviderUrl  string        `env:"GOOGLE_OIDC_PROVIDER_URL,required"`
+	RedirectUri            string        `env:"REDIRECT_URI,required"`
 }
 
 func (appConfig *AppConfig) GetPostgresUrl() *string {
-	return &appConfig.postgresUrl
+	return &appConfig.PostgresUrl
 }
 
 func (appConfig *AppConfig) GetLogLevel() *zapcore.Level {
@@ -26,4 +34,20 @@ func (appConfig *AppConfig) GetLogLevel() *zapcore.Level {
 
 func (appConfig *AppConfig) GetLogToFile() *bool {
 	return &appConfig.LogToFile
+}
+
+func (appConfig *AppConfig) GetGoogleOidcClientId() *string {
+	return &appConfig.GoogleOidcClientId
+}
+
+func (appConfig *AppConfig) GetGoogleOidcClientSecret() *string {
+	return &appConfig.GoogleOidcClientSecret
+}
+
+func (appConfig *AppConfig) GetGoogleOidcProviderUrl() *string {
+	return &appConfig.GoogleOidcProviderUrl
+}
+
+func (appConfig *AppConfig) GetRedirectUri() *string {
+	return &appConfig.RedirectUri
 }
